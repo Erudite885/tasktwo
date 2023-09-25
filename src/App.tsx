b1f3434228e38030a7879cad8c3a1f1bf54fe6c9
Erudite885 <erudite885@gmail.com>
2023-09-25 11:08:45 +0100

    import adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './styles/global.css';
 const App = () => {
   return (
     <Layout>
-      <main className="sticky top-0 l-0 w-full py-[24px] px-[32px] grid grid-flow-col grid-cols-[30%_auto] gap-[32px]">
+      <main className="sticky top-0 left-0 w-full py-[24px] px-[32px] grid grid-flow-col grid-cols-[30%_auto] gap-[32px]">
         <div>
           <Filters />
         </div>
