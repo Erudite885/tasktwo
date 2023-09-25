2314f32b8760e93b746bbec2e7a9fb0eb3a24f36
Erudite885 <erudite885@gmail.com>
2023-09-25 11:14:09 +0100

    filter dynamic component update

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,48 +1,19 @@
-import React from "react";
 import { Input, Collapse } from "antd";
-import ChevronDown from "../../assets/chevron-down.svg";
-import FileMini from "../../assets/file-little.svg";
-import Info from "../../assets/infoicon.svg";
-import Search from "../../assets/search.svg";
 import { filterPagefilters } from "../../constants/index.ts";
+import { ChevronDown, FileMini, Info, Search } from "../../assets/index.ts";
 
 const Filters = () => {
-  const { Panel } = Collapse;
-
-  const onChange = (key) => {
+  const onChange = (key: string | string[]) => {
     console.log(key);
   };
-
-  // const filters = [
-  //   {
-  //     label: "Personal Information",
-  //     dropdown: true,
-  //   },
-  //   {
-  //     label: "Education",
-  //     dropdown: true,
-  //   },
-  //   {
-  //     label: "Work Experience",
-  //     dropdown: true,
-  //   },
-  //   {
-  //     label: "Activity Filter",
-  //     dropdown: true,
-  //   },
-  //   {
-  //     label: "Advanced Filter",
-  //     dropdown: false,
-  //   },
-  // ];
 
   return (
     <div>
       <Input
         placeholder="Search by name, edu, exp or #tag"
         className="!py-[13px] !px-[16px] rounded-[8px] border-[#F3F3F3] mb-[24px]"
-        prefix={ <img src={Search} alt='Search icon' />}
-        suffix={ <img src={Info} alt='Info icon' />}
+        prefix={<img src={Search} alt="Search icon" />}
+        suffix={<img src={Info} alt="Info icon" />}
       />
 
       <div className="bg-white rounded-[8px]">
@@ -52,22 +23,23 @@ const Filters = () => {
         </div>
 
         <Collapse
-          accordion
-          defaultActiveKey={["1"]}
+          items={filterPagefilters.map((filter, index) => {
+            return {
+              key: index,
+              label: filter.label,
+              children: (
+                <p className="text-[#D5D5D5]">Filter options comes here</p>
+              ),
+              extra: <img src={ChevronDown} alt="chevron down" />,
+              className: "!border-[#F2F2F2] rounded-0",
+              onClick: () => {},
+            };
+          })}
+          defaultActiveKey={[]}
           onChange={onChange}
-          expandIcon={() => { <img src={FileMini} alt='FileMini icon' />}}
+          expandIcon={() => <img src={FileMini} alt="file icon" />}
           className="!bg-white !border-[0px]"
-        >
-          {filterPagefilters.map((filter, index) => (
-            <Panel
-              key={index.toString()}
-              header={filter.label}
-              extra= { <img src={ChevronDown} alt='ChevronDown icon' />}
-            >
-              <p className="text-[#d5d5d5]">Filter options come here</p>
-            </Panel>
-          ))}
-        </Collapse>
+        />
       </div>
     </div>
   );
