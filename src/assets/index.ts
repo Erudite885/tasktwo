7211ffece8f03251dca8583f48ef0be8dd8e4982
Erudite885 <erudite885@gmail.com>
2023-09-25 11:09:59 +0100

    asset index file

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,43 @@
-import {  } from "module";
+import Calender from "./calendar.svg";
+import ChevronRight from "./chevron-right.svg";
+import File from "./file.svg";
+import Folder from "./folder.svg";
+import Heart from "./heart.svg";
+import Home from "./home.svg";
+import Settings from "./settings.svg";
+import Share from "./share.svg";
+import Users from "./users.svg";
+import ChevronDownSolid from "./chevron-down-solid.svg";
+import ChevronDownWhite from "./chevron-down-white.svg";
+import Clip from "./clip.svg";
+import ContactBuss from "./contact-buss.svg";
+import ContactCheck from "./contact-check.svg";
+import ContactClose from "./contact-close.svg";
+import Message from "./message.svg";
+import FileMini from "./file-little.svg";
+import Info from "./infoicon.svg";
+import Search from "./search.svg";
+import ChevronDown from "./chevron-down.svg";
+
+export {
+  Calender,
+  ChevronRight,
+  File,
+  Folder,
+  Heart,
+  Home,
+  Settings,
+  Share,
+  Users,
+  ChevronDownSolid,
+  ChevronDownWhite,
+  Clip,
+  ContactBuss,
+  ContactCheck,
+  ContactClose,
+  Message,
+  FileMini,
+  Info,
+  ChevronDown,
+  Search,
+};
