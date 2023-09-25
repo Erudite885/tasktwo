ab60a8ba88742d5c7fca1726cb2386ce7bedae9f
Erudite885 <erudite885@gmail.com>
2023-09-25 11:08:05 +0100

    layout styling adjustment

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,5 @@
-import React from "react";
-import Header from "../Header/Header.tsx";
-// import Header from "./Header/Header.tsx";
- // Import the Header component you created
-import SideNav from "../SideNav/SideNav.tsx"; // Import the SideNav component you created
+
+import { Header, SideNav } from "..";
 
 
 type ILayoutProps = {
@@ -11,7 +8,7 @@ type ILayoutProps = {
 
 function Layout({ children }: ILayoutProps) {
   return (
-    <div className="grid w-full grid-flow-col  grid-cols-[max-content_auto]">
+    <div className="grid w-full grid-flow-col grid-cols-[max-content_auto]">
       <SideNav />
       <div className="flex min-h-screen flex-col items-center justify-between w-full">
         <Header />
